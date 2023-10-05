a56b6f962c6bee622d1078d45706ece22f3e05d0
edohan <edohan0117@gmail.com>
2023-10-05 17:53:11 +0900

    1005/1753

--- frontend/src/pages/TodoItem.vue
+++ frontend/src/pages/TodoItem.vue
@@ -2,11 +2,30 @@
   <div class="main">
     <h1>ToDoList</h1>
     <div class="container">
-      <div class="row">
+      <div class="row g-3">
         <div class="input-group mb-3 justify-content-center mx-auto">
-          <input type="text" class="form-control" placeholder="new todoList" aria-label="new todoList" aria-describedby="button-addon2">
-          <input type="datetime-local" class="form-control" name="time" id="time">
-          <button class="btn btn-outline-secondary" type="button" id="button-addon2">Summit</button>
+          <form class="row g-3">
+            <div class="col-auto">
+              <input type="text" class="form-control" placeholder="new todoList" aria-label="new todoList" aria-describedby="button-addon2">
+            </div>
+            <div class="col-auto">
+              <input type="datetime-local" class="form-control" name="time" id="time">
+            </div>
+            <div class="col-auto">
+              <button class="btn btn-outline-secondary " type="button" id="button-addon2">Summit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="main-list">
+    <div class="container">
+      <div class="listItem">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username with two button addons">
+          <button class="btn btn-outline-secondary" type="button">Button</button>
+          <button class="btn btn-outline-secondary" type="button">Button</button>
         </div>
       </div>
     </div>
