20e1c4099109c681af20f3b7f3a46e887712c474
edohan <edohan0117@gmail.com>
2023-10-05 14:56:36 +0900

    first /

---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import router from './script/router'
+import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+createApp(App)
+    .use(router)    
+    .mount('#app')
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+      <h1>Home Page</h1>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+      <li>aa</li>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
