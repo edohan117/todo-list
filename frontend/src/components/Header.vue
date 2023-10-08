c3e40f0c0e23540c1b3231250dba1025a047c238
edohan <edohan0117@gmail.com>
2023-10-08 19:17:32 +0900

    /

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,13 +5,13 @@
         <div class="row">
           <div class="col-sm-8 col-md-7 py-4">
             <h4>About</h4>
-            <p class="text-body-secondary">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+            <p class="text-body-secondary">Add some information about the album below, the author, or any other background
+              context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to
+              some social networking sites or contact information.</p>
           </div>
           <div class="col-sm-4 offset-md-1 py-4">
-            <h4>Contact</h4>
             <ul class="list-unstyled">
-              <li><a href="#" class="text-white">Follow on Twitter</a></li>
-              <li><a href="#" class="text-white">Like on Facebook</a></li>
+              <li><a href="#" class="text-white" @click="kakaoLogin">Login</a></li>
               <li><a href="#" class="text-white">Email me</a></li>
             </ul>
           </div>
@@ -21,10 +21,16 @@
     <div class="navbar navbar-dark bg-dark shadow-sm">
       <div class="container">
         <a href="#" class="navbar-brand d-flex align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
+            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2"
+            viewBox="0 0 24 24">
+            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+            <circle cx="12" cy="13" r="4" />
+          </svg>
           <strong>Album</strong>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
+          aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       </div>
@@ -34,6 +40,25 @@
 
 <script>
 export default {
+  methods: {
+    kakaoLogin() {
+      window.Kakao.Auth.login({
+        scope: 'profile_nickname, account_email, gender',
+        success: this.getProfile
+      })
+    },
+    getProfile(authObj) {
+      console.log(authObj);
+      window.Kakao.API.request({
+        url: '/v2/user/me',
+        success: res => {
+          const kakao_account = res.kakao_account;
+          console.log(kakao_account);
+          alert("login success");
+        }
+      })
+    }
+  }
 }
 </script>
 
@@ -45,11 +70,13 @@ export default {
   -moz-user-select: none;
   user-select: none;
 }
+
 @media (min-width: 768px) {
-.bd-placeholder-img-lg {
-  font-size: 3.5rem;    
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
 }
-}
+
 .b-example-divider {
   width: 100%;
   height: 3rem;
@@ -58,21 +85,25 @@ export default {
   border-width: 1px 0;
   box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
 }
+
 .b-example-vr {
   flex-shrink: 0;
   width: 1.5rem;
   height: 100vh;
 }
+
 .bi {
   vertical-align: -.125em;
   fill: currentColor;
 }
+
 .nav-scroller {
   position: relative;
   z-index: 2;
   height: 2.75rem;
   overflow-y: hidden;
 }
+
 .nav-scroller .nav {
   display: flex;
   flex-wrap: nowrap;
@@ -99,9 +130,11 @@ export default {
   --bs-btn-active-bg: #5a23c8;
   --bs-btn-active-border-color: #5a23c8;
 }
+
 .bd-mode-toggle {
   z-index: 1500;
 }
+
 .bd-mode-toggle .dropdown-menu .active .bi {
   display: block !important;
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,3 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 createApp(App)
     .use(router)
     .mount('#app')
+
+
+window.Kakao.init("c4f3f6252aaa055e93c941122a261a58");
